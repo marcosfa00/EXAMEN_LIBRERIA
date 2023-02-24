e73b13904d21dc5af166b12414ceda593ce27e54
Marcos F. Avendaño <marcosfavendano@gmail.com>
2023-02-24 09:43:21 +0100

    Estado antes del Merge de la rama Main

--- Readme.md
+++ Readme.md
@@ -3,3 +3,9 @@
 ---
 
 Primero que todo añadimos todo a un repositorio y hacemos el primer commit
+
+TRAS HACER TODOS LOS CAMBIOS OPORTUNOS PROCEDEMOS A REALIZAR EL GIT MERGE CON
+        
+        --squash
+
+Cambiamso a la Rama Main y hacemso el merge
---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import com.marcosfa.entradasalida.EntradaSalida;
+
+/**
+ * rama Main
+ */
+public class Main {
+    /**
+     * Aquí ponemos a prueba los metodos de la clase operacionesbáscias
+     * @param args Argumentos que le pasamos al "Method" main
+     */
+    public static void main(String[] args) {
+        Float resultado = OperacionesBasicas.realizar(OperacionesBasicas.RAIZ,4,16);
+        System.out.println(resultado);
+
+
+    }
+}
